bffc6b21dd332ec034f3d15957102bea63329cde
VinnieStrong <enzoforte990@gmail.com>
2023-04-06 16:37:14 +0200

    Size squares depending page size, begin w sizeGrid function

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 let container = document.querySelector('.container');
 
 function createGrid() {
-    let side=50;
+    let side=20;
     let containerWidth = container.offsetWidth;
     let squareSize = containerWidth/side; 
+    container.style.height = `${containerWidth}px`;
     
     for(let i=0; i<(side*side); i++) {
         let square = document.createElement('div');
         square.classList.add('grid');
         square.setAttribute('style',`width:${squareSize}px;`
         + `height:${squareSize}px;  border : 1px solid black;`
-        + 'box-sizing: border-box; display: flex; '
-        + `flex-basis: 100/${side}%; justify-content:`
-        + 'center; align-items: center;');
+        + 'box-sizing: border-box; display: flex;'
+        + `justify-content:center;align-items:center;`);
         container.appendChild(square);
         square.addEventListener('mousemove', () => { 
         square.style.backgroundColor = 'red';
@@ -21,6 +21,18 @@ function createGrid() {
 }
 createGrid();
 
+function sizeGrid() {
+
+    let value = 20;
+    
+    let domanda = document.querySelector('.prompt');
+    domanda.addEventListener('click', () => {
+    let input =  prompt('How many square per side do'
+    +'you want?')
+    value = parseInt(input) || value;
+    });
+}
+
 function colorGrid() {
     square.addEventListener('mousemove', () => { 
     square.style.backgroundColor = 'red';
@@ -28,18 +40,6 @@ function colorGrid() {
 }
 
 
-let domanda = document.querySelector('.prompt');
-domanda.addEventListener('click', () => {
-    let value =  prompt('How many square per side do'
-    +'you want?', 16)
-});
 
-function changeSquareNumber(value) {
-    if(value<101) {
-        value = side;
-        createGrid();
-    }
-
-}
 
 
